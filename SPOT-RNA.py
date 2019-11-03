bd32ae5c057149f2ee38c2b76877e4459e0da24c
jaswindersingh2 <35057243+jaswindersingh2@users.noreply.github.com>
2019-11-03 16:37:34 +1000

    Update SPOT-RNA.py

--- SPOT-RNA.py
+++ SPOT-RNA.py
@@ -18,8 +18,7 @@ parser.add_argument('--motifs',default=False, type=bool, help='Set this to "True
 args = parser.parse_args()
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-if args.gpu != -1:
-    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+#tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 create_tfr_files(args.inputs)
 
