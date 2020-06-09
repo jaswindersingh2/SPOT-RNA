69201f480260d15987b2096420bfd609c6d21e53
Jaswinder Singh <jaswinder.singh3@griffithuni.edu.au>
2020-06-09 15:14:23 +1000

    minor fix for lower case sequences
    convert lower case nts to upper case and then replace T with U

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def create_tfr_files(all_seq, base_path, input_file):
     with tf.io.TFRecordWriter(path_tfrecords) as writer:
         for i in tqdm(range(len(ids))):
             name     = input_data[2*i].replace(">", "") 
-            sequence = input_data[2*i+1].replace(" ", "").replace("T", "U").upper()
+            sequence = input_data[2*i+1].replace(" ", "").upper().replace("T", "U")
             #print(sequence[-1])
             
             #print(len(sequence), name)                
